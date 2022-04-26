b6474cbc1c47423ab00d7a1fb23f7238ec88cbe0
Eric Grant <senatorericgrant@gmail.com>
2022-04-27 00:08:21 +0700

    refactor: add Tab Bar Controller

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CurrentLocationViewController.swift
 //  MyLocations
 //
 //  Created by Eric Grant on 26.04.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CurrentLocationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
