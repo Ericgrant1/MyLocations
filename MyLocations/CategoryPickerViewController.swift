dd8e6eb64e58a51b5764f4c9a4c02bf09d5e5081
Eric Grant <senatorericgrant@gmail.com>
2022-05-01 21:44:19 +0700

    feat: put the corresponding category name into the selectedCategoryName property

--- MyLocations/CategoryPickerViewController.swift
+++ MyLocations/CategoryPickerViewController.swift
@@ -34,6 +34,19 @@ class CategoryPickerViewController: UITableViewController {
         }
     }
     
+    // MARK: - Navigation
+    override func prepare(
+        for segue: UIStoryboardSegue,
+        sender: Any?
+    ) {
+        if segue.identifier == "PickedCategory" {
+            let cell = sender as! UITableViewCell
+            if let indexPath = tableView.indexPath(for: cell) {
+                selectedCategoryName = categories[indexPath.row]
+            }
+        }
+    }
+    
     // MARK: - Table View Data Source
     override func tableView(
         _ tableView: UITableView,
