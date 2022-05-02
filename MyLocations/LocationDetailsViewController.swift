2d564b914b8f4ab5683d9d4e31dbc456655969ea
Eric Grant <senatorericgrant@gmail.com>
2022-05-02 23:59:13 +0700

    feat: use the HUD view

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -67,6 +67,11 @@ class LocationDetailsViewController: UITableViewController {
     
     // MARK: - Actions
     @IBAction func done() {
+        guard let mainView = navigationController?.parent?.view else {
+            return
+        }
+        let hudView = HudView.hud(inView: mainView, animated: true)
+        hudView.text = "Tagged"
         navigationController?.popViewController(animated: true)
     }
     
