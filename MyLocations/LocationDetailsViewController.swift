79ea059eeb99c5514d71f74f206ebc98b6036288
Eric Grant <senatorericgrant@gmail.com>
2022-05-12 23:15:22 +0700

    feat: choose between camera and photo library

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -150,7 +150,8 @@ class LocationDetailsViewController: UITableViewController {
         if indexPath.section == 0 && indexPath.row == 0 {
             descriptionTextView.becomeFirstResponder()
         } else if indexPath.section == 1 && indexPath.row == 0 {
-            choosePhotoFromLibrary()
+            tableView.deselectRow(at: indexPath, animated: true)
+            pickPhoto()
         }
     }
     
@@ -212,6 +213,42 @@ extension LocationDetailsViewController: UIImagePickerControllerDelegate, UINavi
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func pickPhoto() {
+        if true || UIImagePickerController.isSourceTypeAvailable(.camera) {
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    func showPhotoMenu() {
+        let alert = UIAlertController(
+            title: nil,
+            message: nil,
+            preferredStyle: .actionSheet)
+        
+        let actCancel = UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: nil)
+        alert.addAction(actCancel)
+        
+        let actPhoto = UIAlertAction(
+            title: "Take Photo",
+            style: .default) { _ in
+                self.takePhotoWithCamera()
+            }
+        alert.addAction(actPhoto)
+        
+        let actLibrary = UIAlertAction(
+            title: "Choose From Library",
+            style: .default) { _ in
+                self.choosePhotoFromLibrary()
+            }
+        alert.addAction(actLibrary)
+        present(alert, animated: true, completion: nil)
     }
     
     // MARK: - Image Picker Delegates
